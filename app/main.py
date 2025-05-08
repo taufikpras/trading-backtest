f96b54ba758a6ebb175d78224279665fc9b6d46c
taufikp <taufik.prasetiya@gmail.com>
2025-05-08 07:15:28 +0700

    update new stg

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from backtest import Backtest
 from strategy.strategy_3ema import Strategy_3ema
+from strategy.strategy_ema30 import Strategy_ema30
 from pprint import pprint
 import output
 import warnings
@@ -10,5 +11,9 @@ backtest = Backtest(40000000,0.01)
 stg = Strategy_3ema("3 EMA")
 return_df = backtest.backtest_watchlist(stg)
 output.save_multiple_backtests_to_pdf(return_df,filename=f'{stg.name}.pdf', stg_name=stg.name)
-pprint(return_df)
+
+stg = Strategy_ema30("EMA30 50")
+return_df = backtest.backtest_watchlist(stg)
+output.save_multiple_backtests_to_pdf(return_df,filename=f'{stg.name}.pdf', stg_name=stg.name)
+# pprint(return_df)
 
---
+++ app/strategy/strategy_ema30.py
@@ -0,0 +1,32 @@
+from strategy.strategy import Strategy
+import ta
+
+class Strategy_ema30(Strategy):
+    
+    
+    def add_indicator(self):
+        self.df['ema30'] = ta.trend.EMAIndicator(self.df['Close'], window=30).ema_indicator()
+        self.df['ema50'] = ta.trend.EMAIndicator(self.df['Close'], window=50).ema_indicator()
+        self.df['upper'] = self.df['ema30'] + ta.volatility.AverageTrueRange(self.df['High'], self.df['Low'], self.df['Close'], window=14).average_true_range()
+        self.df['trailing'] = self.df['ema30'].shift(1) - ta.volatility.AverageTrueRange(self.df['High'], self.df['Low'], self.df['Close'], window=14).average_true_range().shift(1)
+        self.df['trailing'] = self.df['trailing'].rolling(window=3).max()
+        self.df['pct'] = (self.df['Close'] - self.df['trailing']) * 100 / self.df['Close']
+        self.df['di'] = ta.trend.ADXIndicator(self.df['High'], self.df['Low'], self.df['Close'], window=14).adx_pos() -  ta.trend.ADXIndicator(self.df['High'], self.df['Low'], self.df['Close'], window=14).adx_neg()
+        self.df = self.df.dropna()
+    
+    def add_signal(self):
+        rule_entry = (
+            (self.df['Close'] < self.df['upper']) &
+            (self.df['Close'] > self.df['trailing']) &
+            (self.df['ema30'] > self.df['ema50']) &
+            (self.df['pct'] < 11) &
+            (self.df['di'] > 0)
+        )
+        self.df['entry_signal'] = rule_entry.astype(int)
+
+        rule_exit = (
+            (self.df['Close'] < self.df['trailing'])
+        )
+        self.df['exit_signal'] = rule_exit.astype(int)
+
+        self.df['exit_partial'] = 0
